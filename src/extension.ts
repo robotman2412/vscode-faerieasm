3a97efaafb835318898ec9fdc304ae43c64ed6f2
RobotMan2412 <julian@scheffers.net>
2024-11-10 11:59:19 +0100

    Syntax definition update

--- src/extension.ts
+++ src/extension.ts
@@ -1,39 +1,51 @@
 import * as path from 'path';
 import { workspace, ExtensionContext } from 'vscode';
+import { DiagnosticFeature } from 'vscode-languageclient/lib/common/diagnostic';
 
 import {
     LanguageClient,
     LanguageClientOptions,
     ServerOptions,
     TransportKind,
-    Executable
+    Executable,
+    DocumentDiagnosticRequest
 } from 'vscode-languageclient/node';
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-    let serverBinary: string|undefined = workspace.getConfiguration("faerieasm").get("assembler_binary")
+    let serverBinary: string|undefined = workspace.getConfiguration("faerieasm").get("assembler_binary");
 
-    if (serverBinary != undefined) {
-        let serverOptions: Executable = {
-            command:   serverBinary,
-            transport: TransportKind.stdio,
-            args:      ["--lsp"]
-        };
-
-        let clientOptions: LanguageClientOptions = {
-            documentSelector: [{ scheme: 'file', language: 'faerieasm' }]
-        };
-
-        client = new LanguageClient(
-            'faerieasm',
-            'Faerie assembly language server',
-            serverOptions,
-            clientOptions
-        );
-
-        client.start();
+    if (serverBinary == undefined || serverBinary == "") {
+        throw Error("No server");
     }
+
+    let serverOptions: Executable = {
+        command:   serverBinary,
+        transport: TransportKind.stdio,
+        args:      ["--lsp"]
+    };
+
+    let clientOptions: LanguageClientOptions = {
+        documentSelector: [{ scheme: 'file', language: 'faerieasm' }]
+    };
+
+    client = new LanguageClient(
+        'faerieasm',
+        'Faerie assembly language server',
+        serverOptions,
+        clientOptions
+    );
+
+    client.start();
+    let diagnosticReq = client.getFeature(DocumentDiagnosticRequest.method);
+    if (diagnosticReq == undefined) {
+        throw Error("No diagnostic request feature");
+    }
+    
+    workspace.onDidSaveTextDocument((doc) => {
+        let shape = diagnosticReq.getProvider(doc);
+    })
 }
 
 export function deactivate(): Thenable<void> | undefined {
